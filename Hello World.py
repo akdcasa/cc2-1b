8b0447c50dc661a158bc95599d0d8f1d2ab8c7dc
Casa, Aldwin Keith <145945123+akdcasa@users.noreply.github.com>
2023-09-27 08:50:15 +0800

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,17 @@
+
+# enter name
+name = input("what is your name?")
+
+# age converter input in to integer
+age=int(input("what is your age?"))
+
+module = 5%3
+
+pi = 3.1416
+
+print("Hello , "+name)
+print("Hello ,",name)
+print(f"Hello , {name}")
+print(f"My age next year is keith{(+age+1)}")
+print(f"pi value is {+pi}")
+print(module)
