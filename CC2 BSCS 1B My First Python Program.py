c302efff8de5961d3c099f3d3bbe194de475cad4
Casa, Aldwin Keith <145945123+akdcasa@users.noreply.github.com>
2023-09-29 08:03:59 +0800

    labact1-My-First-Python-Program.py

---
+++ CC2 BSCS 1B My First Python Program.py
@@ -0,0 +1,50 @@
+#Casa,Aldwin Keith D. BSCS 1B
+
+#weight to kilograms
+pounds = eval(input("Weight in Pounds (lbs):"))
+conversion = 2.205
+kilograms = (pounds) / (conversion)
+print("Weight converted to Kilograms (km):",kilograms)
+print("=====================================================")
+#leght to kilometer
+Miles = eval(input("Lenght in Miles (mi):"))
+conversion = 1.609344
+kilometers = (Miles) * (conversion)
+print("Lenght converted to Kilometers (km):",kilometers)
+print("=====================================================")
+#fahrenhiet to celsius
+F = eval(input("Temperature in Fahrenheit (°F):"))
+conversion = -32
+Fahrenheit = (F - 32) * 5/9
+print("Temperature in Celsius (°C) =",Fahrenheit)
+print("=====================================================")
+#average of the numbers
+a = eval(input("Age of the student 1:"))
+b = eval(input("Age of the student 2:"))
+c = eval(input("Age of the student 3:"))
+d = eval(input("Age of the student 4:"))
+e = eval(input("Age of the student 5:"))
+f = eval(input("Age of the student 6:"))
+g = eval(input("Age of the student 7:"))
+h = eval(input("Age of the student 8:"))
+i = eval(input("Age of the student 9:"))
+j = eval(input("Age of the student 10:"))
+add = a+b+c+d+e+f+g+h+i+j
+avg = add / 10
+print("The average age of the student is:", avg)
+print("====================================================")
+print("The Farlight ground Base in Earth 2.0, there is a constructed spacecraft going home covered with high walls and heroes.")
+print("Suddenly there was a loud bang <Triple Rocket launcher boowmm>, destroying the wall of the base.")
+print("The base is under attack by troops of robots under MK.R that has a rapid gun <Jupiter Assault Riffle> that can destroy everything.")
+print("Everything on their front was destroyed and getting closer, but the heroes came and protected the spacecraft.")
+print("Here comes Maggie who has a jetpack boost and reloads her gun instantly, <taktical roll goess brrr> that dominates half of the troops.")
+print("Suddenly she got shot but it didn't kill her, thanks to Ceanna her friend who deployed a device recovery<Retoration Field> that can provide healing and shield repair ")
+print("and has a drone that shoots, but suddenly they got wasted by MK.R.")
+print("While MK.R is busy with them, Phantom their sniper friend takes a shot at his head <Sniper Fatal Shot head go boom> from the distance that,")
+print("makes all enemy troops shut down and protects the spacecraft going home to the real Earth.")
+input()
+input()
+input()
+input()
+input()
+input ("Press enter to close...")
